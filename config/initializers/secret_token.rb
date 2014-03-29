77939082b066d50a29a12cdeafd24fcd9caf98ef
sugeng tigefa <sugeng@tigefa.org>
2014-03-30 06:31:23 +0700

    re Initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tigefa::Application.config.secret_key_base = 'ec1ee1a3f30a4cc098b7dd436d0c92ace720161c034e9845a78d69870afe2282832dcae0cc921b8acfaa94cbfdd18906eacaa40a0c531c0a4a4393736c84de6c'
